e77fdef11bcd9b397ddac2f0e77ab7965ddab32d
Yingqi Chen <chanwk18@hotmail.com>
2019-03-23 07:39:08 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -12,6 +12,7 @@ def line(katz_deli)
   if katz_deli==[]
     puts "The line is currently empty."
   else
+    array=[]
     katz_deli.collect.with_index do |name,index|
       array<< "#{index+1}. #{name} "
     end
