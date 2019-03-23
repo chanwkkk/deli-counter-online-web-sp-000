72db3bd74e359cdbd934690e09b6984d56393b5a
Yingqi Chen <chanwk18@hotmail.com>
2019-03-23 07:42:16 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -13,7 +13,7 @@ def line(katz_deli)
     puts "The line is currently empty."
   else
     array=[]
-    katz_deli.collect.with_index do |name,index|
+    katz_deli.each_with_index do |name,index|
       array<< "#{index+1}. #{name}"
     end
     string=array.join(" ")
